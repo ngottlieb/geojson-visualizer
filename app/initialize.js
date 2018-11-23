b4e8881b43ad204962f766a2db5bb3c93a09f2bd
Nick Gottlieb <ngottlieb@gmail.com>
2018-11-23 08:11:07 -0700

    initial commit w/ skeleton, bootstrap, and leaflet-ajax

---
+++ app/initialize.js
@@ -0,0 +1,6 @@
+import Map from 'map';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const map = new Map;
+  console.log('Initialized app');
+});
---
+++ brunch-config.js
@@ -0,0 +1,26 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
+      'app.js': /^app/
+    }
+  },
+  stylesheets: {joinTo: 'app.css'}
+};
+
+exports.plugins = {
+  babel: { presets: ['latest'] },
+  copycat: {
+    "images": ["app/assets/images", "node_modules/leaflet/dist/images"],
+    verbose: true,
+    onlyChanged: true
+  }
+};
+
+exports.npm = {
+  styles: {
+    'leaflet': ['dist/leaflet.css'],
+    'bootstrap': ['dist/css/bootstrap.css']
+  },
+};
