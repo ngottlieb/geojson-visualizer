f490e1f8d31488d655937cf8328b693424951262
Nick Gottlieb <ngottlieb@gmail.com>
2018-12-17 17:58:01 -0800

    add auto change of tab once loaded

--- app/components/DataForm.js
+++ app/components/DataForm.js
@@ -1,33 +1,49 @@
-import React from 'react';
-import { MapControl } from 'react-leaflet';
-import { Well, Form, FormGroup, FormControl, Checkbox, ControlLabel, Button, Tabs, Tab } from 'react-bootstrap';
-import Info from './Info';
-import Filters from './Filters';
+import React from "react";
+import {MapControl} from "react-leaflet";
+import {
+  Well,
+  Form,
+  FormGroup,
+  FormControl,
+  Checkbox,
+  ControlLabel,
+  Button,
+  Tabs,
+  Tab,
+} from "react-bootstrap";
+import Info from "./Info";
+import Filters from "./Filters";
 
 export default class DataForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataUrl: ''
-    }
+      dataUrl: "",
+      activeTabKey: this.props.geoJSON ? 2 : 1,
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.loadData = this.loadData.bind(this);
-
   }
 
   handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({[e.target.name]: e.target.value});
   }
 
   loadData() {
     this.props.updateDataUrl(this.state.dataUrl);
+    this.setState({activeTabKey: 2});
   }
-  
+
   render() {
     return (
-      <Well className='form-box leaflet-top leaflet-control leaflet-right'>
-        <Tabs defaultActiveKey={1} id="data-form-tabs">
+      <Well className="form-box leaflet-top leaflet-control leaflet-right">
+        <Tabs
+          activeKey={this.state.activeTabKey}
+          id="data-form-tabs"
+          onSelect={key => {
+            this.setState({activeTabKey: key});
+          }}>
           <Tab eventKey={1} title="Load Data">
             <Form>
               <FormGroup controlId="dataUrl">
@@ -40,21 +56,16 @@ export default class DataForm extends React.Component {
                   onChange={this.handleChange}
                 />
               </FormGroup>
-              <Button
-                bsStyle="info"
-                onClick={this.loadData}>Load Data</Button>
+              <Button bsStyle="info" onClick={this.loadData}>
+                Load Data
+              </Button>
             </Form>
           </Tab>
           <Tab eventKey={2} title="Info" className="info-tab">
-            <Info
-              className="info-tab"
-              geoJSON={this.props.geoJSON}
-            />
+            <Info className="info-tab" geoJSON={this.props.geoJSON} />
           </Tab>
           <Tab eventKey={3} title="Filter">
-            <Filters
-              geoJSON={this.props.geoJSON}
-            />
+            <Filters geoJSON={this.props.geoJSON} />
           </Tab>
         </Tabs>
       </Well>
